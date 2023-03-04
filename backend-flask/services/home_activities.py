d20ca40b6cde4ab45409e74bc65d0221bc3a9189
ElsieMarion <elcymao@gmail.com>
2023-03-04 08:26:09 +0000

    spanning in honeycomb

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -12,7 +12,7 @@ class HomeActivities:
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Miss Ess',
-        'message': 'Cloud is very fun!',
+        'message': ' Yeah Cloud is very fun!',
         'created_at': (now - timedelta(days=2)).isoformat(),
         'expires_at': (now + timedelta(days=5)).isoformat(),
         'likes_count': 5,
@@ -22,7 +22,7 @@ class HomeActivities:
           'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
           'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
           'handle':  'Worf',
-          'message': 'This post has no honor!',
+          'message': 'Great job getting started!',
           'likes_count': 0,
           'replies_count': 0,
           'reposts_count': 0,
@@ -31,7 +31,7 @@ class HomeActivities:
       },
       {
         'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-        'handle':  'Worf',
+        'handle':  'Worftune',
         'message': 'I am out of prune juice',
         'created_at': (now - timedelta(days=7)).isoformat(),
         'expires_at': (now + timedelta(days=9)).isoformat(),
@@ -40,8 +40,8 @@ class HomeActivities:
       },
       {
         'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Garek',
-        'message': 'My dear doctor, I am just simple tailor',
+        'handle':  'Shawry',
+        'message': 'Nice suit mate, l like your creativity',
         'created_at': (now - timedelta(hours=1)).isoformat(),
         'expires_at': (now + timedelta(hours=12)).isoformat(),
         'likes': 0,
