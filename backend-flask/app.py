3d76dd5f63bc288fc98cbe7346280cf2defbc25a
ElsieMarion <elcymao@gmail.com>
2023-03-18 12:17:19 +0000

    deploying envoy as a sidecar with flask backend

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -1,114 +1,250 @@
-import time
-import requests
-from jose import jwk, jwt
-from jose.exceptions import JOSEError
-from jose.utils import base64url_decode
+from flask import Flask
+from flask import request
+from flask_cors import CORS, cross_origin
 
-class FlaskAWSCognitoError(Exception):
-  pass
+# using envoy for jwt verify process
+from flask_restful import Resource, Api
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 
-class TokenVerifyError(Exception):
-  pass
+import os
+import sys
 
-def extract_access_token(request_headers):
-    access_token = None
-    auth_header = request_headers.get("Authorization")
-    if auth_header and " " in auth_header:
-        _, access_token = auth_header.split()
-    return access_token
+from services.home_activities import *
+from services.user_activities import *
+from services.create_activity import *
+from services.create_reply import *
+from services.search_activities import *
+from services.message_groups import *
+from services.messages import *
+from services.create_message import *
+from services.show_activity import *
 
-class CognitoJwtToken:
-    def __init__(self, user_pool_id, user_pool_client_id, region, request_client=None):
-        self.region = region
-        if not self.region:
-            raise FlaskAWSCognitoError("No AWS region provided")
-        self.user_pool_id = user_pool_id
-        self.user_pool_client_id = user_pool_client_id
-        self.claims = None
-        if not request_client:
-            self.request_client = requests.get
-        else:
-            self.request_client = request_client
-        self._load_jwk_keys()
+from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
+
+# HoneyComb ---------
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+
+# X-RAY ----------
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+# CloudWatch Logs ----
+import watchtower
+import logging
+
+# Rollbar ------
+from time import strftime
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
+# Configuring Logger to Use CloudWatch
+# LOGGER = logging.getLogger(__name__)
+# LOGGER.setLevel(logging.DEBUG)
+# console_handler = logging.StreamHandler()
+# cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+# LOGGER.addHandler(console_handler)
+# LOGGER.addHandler(cw_handler)
+# LOGGER.info("test log")
+
+# HoneyComb ---------
+# Initialize tracing and an exporter that can send data to Honeycomb
+provider = TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+
+# X-RAY ----------
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+
+# OTEL ----------
+# Show this in the logs within the backend-flask app (STDOUT)
+#simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+#provider.add_span_processor(simple_processor)
+
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
+app = Flask(__name__)
+
+#envoy endpoints
+api = Api(app)
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this to a secure secret key
+jwt = JWTManager(app)
+
+class SecretResource(Resource):
+    @jwt_required
+    def get(self):
+        current_user = get_jwt_identity()
+        return {'message': f'Hello {current_user}!'}
+
+api.add_resource(SecretResource, '/secret')
+
+# cognito_jwt_token = CognitoJwtToken(
+#   user_pool_id=os.getenv("AWS_COGNITO_USER_POOL_ID"), 
+#   user_pool_client_id=os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
+#   region=os.getenv("AWS_REGION")
+# )
+
+# X-RAY ----------
+XRayMiddleware(app, xray_recorder)
+
+# HoneyComb ---------
+# Initialize automatic instrumentation with Flask
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
 
 
-    def _load_jwk_keys(self):
-        keys_url = f"https://cognito-idp.{self.region}.amazonaws.com/{self.user_pool_id}/.well-known/jwks.json"
-        try:
-            response = self.request_client(keys_url)
-            self.jwk_keys = response.json()["keys"]
-        except requests.exceptions.RequestException as e:
-            raise FlaskAWSCognitoError(str(e)) from e
+frontend = os.getenv('FRONTEND_URL')
+backend = os.getenv('BACKEND_URL')
+origins = [frontend, backend]
+cors = CORS(
+  app, 
+  resources={r"/api/*": {"origins": origins}},
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
+  methods="OPTIONS,GET,HEAD,POST"
+)
 
-    @staticmethod
-    def _extract_headers(token):
-        try:
-            headers = jwt.get_unverified_headers(token)
-            return headers
-        except JOSEError as e:
-            raise TokenVerifyError(str(e)) from e
+# CloudWatch Logs -----
+#@app.after_request
+#def after_request(response):
+#    timestamp = strftime('[%Y-%b-%d %H:%M]')
+#    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+#    return response
 
-    def _find_pkey(self, headers):
-        kid = headers["kid"]
-        # search for the kid in the downloaded public keys
-        key_index = -1
-        for i in range(len(self.jwk_keys)):
-            if kid == self.jwk_keys[i]["kid"]:
-                key_index = i
-                break
-        if key_index == -1:
-            raise TokenVerifyError("Public key not found in jwks.json")
-        return self.jwk_keys[key_index]
+# Rollbar ----------
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
 
-    @staticmethod
-    def _verify_signature(token, pkey_data):
-        try:
-            # construct the public key
-            public_key = jwk.construct(pkey_data)
-        except JOSEError as e:
-            raise TokenVerifyError(str(e)) from e
-        # get the last two sections of the token,
-        # message and signature (encoded in base64)
-        message, encoded_signature = str(token).rsplit(".", 1)
-        # decode the signature
-        decoded_signature = base64url_decode(encoded_signature.encode("utf-8"))
-        # verify the signature
-        if not public_key.verify(message.encode("utf8"), decoded_signature):
-            raise TokenVerifyError("Signature verification failed")
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
-    @staticmethod
-    def _extract_claims(token):
-        try:
-            claims = jwt.get_unverified_claims(token)
-            return claims
-        except JOSEError as e:
-            raise TokenVerifyError(str(e)) from e
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
-    @staticmethod
-    def _check_expiration(claims, current_time):
-        if not current_time:
-            current_time = time.time()
-        if current_time > claims["exp"]:
-            raise TokenVerifyError("Token is expired")  # probably another exception
+@app.route("/api/message_groups", methods=['GET'])
+def data_message_groups():
+  user_handle  = 'andrewbrown'
+  model = MessageGroups.run(user_handle=user_handle)
+  if model['errors'] is not None:
+    return model['errors'], 422
+  else:
+    return model['data'], 200
 
-    def _check_audience(self, claims):
-        # and the Audience  (use claims['client_id'] if verifying an access token)
-        audience = claims["aud"] if "aud" in claims else claims["client_id"]
-        if audience != self.user_pool_client_id:
-            raise TokenVerifyError("Token was not issued for this audience")
+@app.route("/api/messages/@<string:handle>", methods=['GET'])
+def data_messages(handle):
+  user_sender_handle = 'andrewbrown'
+  user_receiver_handle = request.args.get('user_reciever_handle')
 
-    def verify(self, token, current_time=None):
-        """ https://github.com/awslabs/aws-support-tools/blob/master/Cognito/decode-verify-jwt/decode-verify-jwt.py """
-        if not token:
-            raise TokenVerifyError("No token provided")
+  model = Messages.run(user_sender_handle=user_sender_handle, user_receiver_handle=user_receiver_handle)
+  if model['errors'] is not None:
+    return model['errors'], 422
+  else:
+    return model['data'], 200
+  return
 
-        headers = self._extract_headers(token)
-        pkey_data = self._find_pkey(headers)
-        self._verify_signature(token, pkey_data)
+@app.route("/api/messages", methods=['POST','OPTIONS'])
+@cross_origin()
+def data_create_message():
+  user_sender_handle = 'andrewbrown'
+  user_receiver_handle = request.json['user_receiver_handle']
+  message = request.json['message']
 
-        claims = self._extract_claims(token)
-        self._check_expiration(claims, current_time)
-        self._check_audience(claims)
+  model = CreateMessage.run(message=message,user_sender_handle=user_sender_handle,user_receiver_handle=user_receiver_handle)
+  if model['errors'] is not None:
+    return model['errors'], 422
+  else:
+    return model['data'], 200
+  return
 
-        self.claims = claims 
-        return claims
+@app.route("/api/activities/home", methods=['GET'])
+@xray_recorder.capture('activities_home')
+def data_home():
+  access_token = extract_access_token(request.headers)
+  try:
+    claims = cognito_jwt_token.verify(access_token)
+    # authenicatied request
+    app.logger.debug("authenicated")
+    app.logger.debug(claims)
+    app.logger.debug(claims['username'])
+    data = HomeActivities.run(cognito_user_id=claims['username'])
+  except TokenVerifyError as e:
+    # unauthenicatied request
+    app.logger.debug(e)
+    app.logger.debug("unauthenicated")
+    data = HomeActivities.run()
+  return data, 200
+
+@app.route("/api/activities/@<string:handle>", methods=['GET'])
+@xray_recorder.capture('activities_users')
+def data_handle(handle):
+  model = UserActivities.run(handle)
+  if model['errors'] is not None:
+    return model['errors'], 422
+  else:
+    return model['data'], 200
+
+@app.route("/api/activities/search", methods=['GET'])
+def data_search():
+  term = request.args.get('term')
+  model = SearchActivities.run(term)
+  if model['errors'] is not None:
+    return model['errors'], 422
+  else:
+    return model['data'], 200
+  return
+
+@app.route("/api/activities", methods=['POST','OPTIONS'])
+@cross_origin()
+def data_activities():
+  user_handle  = 'andrewbrown'
+  message = request.json['message']
+  ttl = request.json['ttl']
+  model = CreateActivity.run(message, user_handle, ttl)
+  if model['errors'] is not None:
+    return model['errors'], 422
+  else:
+    return model['data'], 200
+  return
+
+@app.route("/api/activities/<string:activity_uuid>", methods=['GET'])
+@xray_recorder.capture('activities_show')
+def data_show_activity(activity_uuid):
+  data = ShowActivity.run(activity_uuid=activity_uuid)
+  return data, 200
+
+@app.route("/api/activities/<string:activity_uuid>/reply", methods=['POST','OPTIONS'])
+@cross_origin()
+def data_activities_reply(activity_uuid):
+  user_handle  = 'andrewbrown'
+  message = request.json['message']
+  model = CreateReply.run(message, user_handle, activity_uuid)
+  if model['errors'] is not None:
+    return model['errors'], 422
+  else:
+    return model['data'], 200
+  return
+
+if __name__ == "__main__":
+  app.run(debug=True)
