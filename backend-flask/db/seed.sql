1054047e779dade8879a6f9436219f78e46522cc
ElsieMarion <elcymao@gmail.com>
2023-03-23 10:28:55 +0000

    database scripts

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
+  ('Elcy Marion', 'elcymarion' ,'MOCK'),
+  ('Apunya Marista', 'punya' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
