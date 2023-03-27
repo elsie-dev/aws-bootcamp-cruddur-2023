2cc3533a6e5c53be1f250828091c370c7c65f6b3
ElsieMarion <elcymao@gmail.com>
2023-03-27 09:19:56 +0000

    script to reload db schema

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,7 @@
 INSERT INTO public.users (display_name, handle, cognito_user_id)
 VALUES
   ('Elcy Marion', 'elcymarion' ,'MOCK'),
-  ('Apunya Marista', 'punya' ,'MOCK');
+  ('Kaelie Mochi', 'mochi' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
