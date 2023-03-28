50eb5a847d562f48cc2e0b89aa5471f0e6f003f4
ElsieMarion <elcymao@gmail.com>
2023-03-28 09:24:11 +0000

    updating database schema

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, cognito_user_id)
 VALUES
-  ('Elcy Marion', 'elcymarion' ,'MOCK'),
-  ('Kaelie Mochi', 'mochi' ,'MOCK');
+  ('elisemarion', 'marion' ,'MOCK'),
+  ('DanielFrozen', 'frozen' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
