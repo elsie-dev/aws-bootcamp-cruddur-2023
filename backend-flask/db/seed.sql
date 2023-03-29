b304ad13134af6a6cf98352f42040fc38ca87df1
ElsieMarion <elcymao@gmail.com>
2023-03-29 05:08:43 +0000

    postgres queries

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('elisemarion', 'marion' ,'MOCK'),
-  ('DanielFrozen', 'frozen' ,'MOCK');
+  ('elise marion','elcy@gmail.com', 'marion' ,'MOCK'),
+  ('Daniel Frozen', 'danny@gmail.com', 'frozen' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
