8aea6a06ff62fd494b25a16965bf717ab0c2d4ac
ElsieMarion <elcymao@gmail.com>
2023-04-01 16:15:43 +0000

    bash scripts

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -208,7 +208,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  =
+  user_handle  = ""
   request.json["user_handle"]
   message = request.json['message']
   ttl = request.json['ttl']
